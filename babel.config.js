e32a5221019c910a3991667d44a07ac5ffb24e4b
wellington <wellingtonslpbi@hotmail.com>
2021-01-20 09:39:27 -0300

    adjust to have style rule condition test

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,7 @@ module.exports = {
       [
         'babel-plugin-styled-components',
         {
-          'displayName': true,
+          'displayName': false,
           'ssr': false
         }
       ]
